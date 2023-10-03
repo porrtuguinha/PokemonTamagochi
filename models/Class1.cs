c67486ba01c29237700434d780bfb43aae4a7a57
Thiago Henrique Bortolozo <132089158+porrtuguinha@users.noreply.github.com>
2023-10-03 15:56:52 -0300

    Add files via upload

---
+++ models/Class1.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace pokemonAgoraVai.models
+{
+    internal class ChamaPokemon
+    {
+        Pokemon2 pokemon1;
+
+        public async void chamaPokemon(string pokemon)
+        {
+
+            string link = $"https://pokeapi.co/api/v2/pokemon/{pokemon}";
+            var rest = new RestClient(link);
+            var resposta = new RestRequest(link, Method.Get);
+            var request = rest.ExecuteGetAsync(resposta);
+            pokemon1 = JsonSerializer.Deserialize<Pokemon2>(request.Result.Content);
+        }
+
+        internal void chamaPokemon(object pokemonEscolhido)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
